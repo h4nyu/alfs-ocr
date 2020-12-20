f1d1e5e3915581f0b936d9c7a1aaf6e18fca4fba
xinyuan-yao <yao.ntno@gmail.com>
2020-12-20 22:36:38 +0000

    revert to flask

--- alfs_char/web.py
+++ alfs_char/web.py
@@ -1,33 +1,28 @@
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from typing import List, Any
 import torch
 import numpy as np
-from uvicorn import Config, Server
 from PIL import Image as PILImage
 import base64
 from torchvision.transforms import ToPILImage
 from io import BytesIO
 import base64
 import asyncio
-from starlette.middleware.cors import CORSMiddleware
 from alfs_char.train import model, model_loader, to_boxes
 from alfs_char.data import test_transforms
 from alfs_char import config
 
-app = FastAPI()
-
-
-class UploadPayload(BaseModel):
-    data: str
+app = Flask(__name__)
+CORS(app)
 
 model_loader.load_if_needed(model)
 device = torch.device("cuda")
 
 @torch.no_grad()
-def detect(payload: UploadPayload) -> Any:
+def detect() -> Any:
     image = np.array(
-        PILImage.open(BytesIO(base64.b64decode(payload.data))).convert("RGB")
+        PILImage.open(BytesIO(base64.b64decode(request.json['data']))).convert("RGB")
     )
     img_tensor = test_transforms(image=image, labels=[], bboxes=[])["image"]
     batch = torch.stack([img_tensor / 255]).to(device)
@@ -38,31 +33,14 @@ def detect(payload: UploadPayload) -> Any:
     out_img = ToPILImage()(batch[0].cpu())
     buffer = BytesIO()
     out_img.save(buffer, format="JPEG")
-    return {
-        "image": base64.b64encode(buffer.getvalue()),
-        "boxes": out_boxes.tolist(),
-        "scores": out_scores.tolist(),
-    }
+    return jsonify(
+        image=base64.b64encode(buffer.getvalue()).decode('utf-8'),
+        boxes=out_boxes.tolist(),
+        scores=out_scores.tolist(),
+    )
 
 
-app.post("/api/upload-image")(detect)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.route("/api/upload-image", methods=["POST"])(detect)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    server = Server(
-        config=Config(
-            app=app,
-            host="0.0.0.0",
-            port=5000,
-            loop=loop,
-            log_level="info",
-        )
-    )
-    loop.run_until_complete(server.serve())
+    app.run(debug=True, host="0.0.0.0", port=5000)
