95ea9d76623899a1b42ae545f44110b6ed70124a
xinyuan-yao <yao.ntno@gmail.com>
2021-03-21 02:56:39 +0000

    add l4t

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/shell
 
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_REGISTRY/alfs/char-recog --push .
+# docker buildx build --platform linux/amd64 --tag $DOCKER_REGISTRY/alfs/char-recog --push .
+docker buildx build --platform linux/arm64 --tag $DOCKER_REGISTRY/alfs/char-recog --push . -f Dockerfile.l4t
