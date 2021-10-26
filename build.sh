1b91fddca049478e93ba95e65083a05d3a3cfcc2
yao <yao.ntno@gmail.com>
2021-10-26 15:15:24 +0000

    fix build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 ARCH=$(uname -m)
 echo $ARCH
 case $ARCH in
-  x86_64)   docker buildx build --platform linux/amd64 --tag alfs-ocr --push . -f Dockerfile ;;
-  aarch64)   docker buildx build --platform linux/arm64 --tag alfs-ocr --push . -f Dockerfile.l4t ;;
+  x86_64)   docker buildx build --platform linux/amd64 --tag alfs-ocr . -f Dockerfile ;;
+  aarch64)   docker buildx build --platform linux/arm64 --tag alfs-ocr . -f Dockerfile.l4t ;;
   *)        echo unexpected arch $ARCH ;;
 esac
