3b1e682c9cbfcb30daa2197949a80c037fe0ed40
h4nyu <yao.ntno@gmail.com>
2022-01-12 02:17:08 +0900

    train

--- alfs_char/data.py
+++ alfs_char/data.py
@@ -1,12 +1,13 @@
 import torch
+from torch import Tensor
 from typing import *
-from typing_extensions import Literal
 from torch.utils.data import Dataset
 from toolz import map
 import numpy as np
 
 import base64
 from io import BytesIO
+import cv2
 from .store import ImageRepository
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
@@ -15,6 +16,8 @@ from PIL import Image as PILImage
 import os
 import albumentations as albm
 from .store import Rows
+from vision_tools.interface import TrainBatch, TrainSample
+from vision_tools.box import resize_boxes
 
 bbox_params = {"format": "pascal_voc", "label_fields": ["labels"]}
 
@@ -26,3 +29,118 @@ Transform = lambda image_size: A.Compose(
     ],
     bbox_params=bbox_params,
 )
+
+TrainTransform = lambda image_size: A.Compose(
+    [
+        A.PadIfNeeded(
+            min_width=image_size,
+            min_height=image_size,
+            border_mode=cv2.BORDER_CONSTANT,
+            p=0.5,
+        ),
+        A.Rotate(limit=(-5, 5), p=1.0, border_mode=0),
+        A.OneOf(
+            [
+                A.Blur(blur_limit=7, p=0.5),
+                A.MotionBlur(blur_limit=7, p=0.5),
+                A.MotionBlur(blur_limit=13, p=0.5),
+            ],
+            p=0.4,
+        ),
+        A.OneOf(
+            [
+                A.IAAAdditiveGaussianNoise(),
+                A.GaussNoise(),
+            ],
+            p=0.2,
+        ),
+        A.Cutout(),
+        A.ColorJitter(p=0.2),
+        albm.RandomBrightnessContrast(),
+        A.HueSaturationValue(
+            p=0.3,
+            hue_shift_limit=15,
+            sat_shift_limit=20,
+            val_shift_limit=15,
+        ),
+        A.OneOf(
+            [
+                A.RandomSizedBBoxSafeCrop(
+                    height=image_size,
+                    width=image_size,
+                    p=1.0,
+                ),
+                A.RandomResizedCrop(height=image_size, width=image_size),
+            ],
+            p=1.0,
+        ),
+        ToTensorV2(),
+    ],
+    bbox_params=bbox_params,
+)
+
+
+class TrainDataset(Dataset):
+    def __init__(
+        self,
+        repo: ImageRepository,
+        rows: Rows,
+        transform: Any,
+    ) -> None:
+        self.repo = repo
+        self.rows = rows
+        self.transform = transform
+
+    def __getitem__(self, idx: int) -> TrainSample:
+        id = self.rows[idx]["id"]
+        res = self.repo.find(id)
+        image = (
+            np.array(
+                PILImage.open(BytesIO(base64.b64decode(res["data"]))).convert("RGB")
+            )
+        )
+        h, w, _ = image.shape
+        boxes = clip_boxes_to_image(
+            torch.tensor(
+                [
+                    [
+                        b["x0"],
+                        b["y0"],
+                        b["x1"],
+                        b["y1"],
+                    ]
+                    for b in res["boxes"]
+                ]
+            ),
+            (1.0, 1.0),
+        )
+        indices = remove_small_boxes(boxes, 0.00001)
+        boxes = resize_boxes(boxes[indices], (w, h))
+        labels = torch.tensor([0 for b in boxes])
+        transed = self.transform(image=image, bboxes=boxes, labels=labels)
+        return dict(
+            id=id,
+            image=transed["image"] / 255,
+            boxes=torch.tensor(transed["bboxes"]),
+            labels=torch.tensor(transed["labels"]),
+        )
+
+    def __len__(self) -> int:
+        return len(self.rows)
+
+
+def collate_fn(
+    batch: list[TrainSample],
+) -> TrainBatch:
+    images: list[Tensor] = []
+    box_batch: list[Tensor] = []
+    label_batch: list[Tensor] = []
+    for row in batch:
+        images.append(row["image"])
+        box_batch.append(row["boxes"])
+        label_batch.append(row["labels"])
+    return TrainBatch(
+        image_batch=torch.stack(images),
+        box_batch=box_batch,
+        label_batch=label_batch,
+    )
---
+++ tests/test_yolox.py
@@ -0,0 +1,85 @@
+import pytest
+import torch
+import os
+from omegaconf import OmegaConf
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.ops import box_convert
+from alfs_char.yolox import get_model, get_criterion, get_checkpoint, get_writer
+from alfs_char.store import ImageRepository
+from vision_tools.assign import SimOTA
+from torch.utils.data import DataLoader
+from vision_tools.yolox import YOLOX, Criterion
+from vision_tools.interface import TrainBatch
+from vision_tools.utils import draw, batch_draw, seed_everything
+from alfs_char.data import (
+    TrainDataset,
+    TrainTransform,
+    Transform,
+    collate_fn,
+)
+
+cfg = OmegaConf.load("/app/kuzushiji_bench/config/yolox.yaml")
+writer = get_writer(cfg)
+
+seed_everything()
+
+
+@pytest.fixture
+def model() -> YOLOX:
+    cfg.score_threshold = 0.5
+    cfg.box_iou_threshold = 0.2
+    m = get_model(cfg)
+    checkpoint = get_checkpoint(cfg)
+    checkpoint.load_if_exists(
+        m,
+    )
+    return m
+
+
+@pytest.fixture
+def criterion() -> Criterion:
+    return get_criterion(cfg)
+
+
+@pytest.fixture
+def batch() -> TrainBatch:
+    repo = ImageRepository()
+    rows = repo.filter()
+    transform = Transform(cfg.image_size)
+    dataset = TrainDataset(repo, rows, transform)
+    loader_iter = iter(DataLoader(dataset, collate_fn=collate_fn, batch_size=1))
+    return next(loader_iter)
+
+
+@torch.no_grad()
+def test_assign(batch: TrainBatch, model: YOLOX, criterion: Criterion) -> None:
+    gt_box_batch = batch["box_batch"]
+    gt_label_batch = batch["label_batch"]
+    image_batch = batch["image_batch"]
+    feats = model.feats(image_batch)
+    pred_yolo_batch = model.box_branch(feats)
+    gt_yolo_batch, pos_idx = criterion.prepeare_box_gt(
+        model.num_classes, gt_box_batch, gt_label_batch, pred_yolo_batch
+    )
+    box_batch = [
+        box_convert(b[idx], in_fmt="cxcywh", out_fmt="xyxy")
+        for b, idx in zip(gt_yolo_batch[:, :, :4], pos_idx)
+    ]
+    plot = batch_draw(image_batch=image_batch, box_batch=box_batch)
+    writer.add_image("assign", plot, 0)
+    box_batch = [
+        box_convert(b[idx], in_fmt="cxcywh", out_fmt="xyxy")
+        for b, idx in zip(pred_yolo_batch[:, :, :4], pos_idx)
+    ]
+    point_batch = [
+        b[idx].view(len(b[idx]), 1, 2)
+        for b, idx in zip(pred_yolo_batch[:, :, 7:9], pos_idx)
+    ]
+    plot = batch_draw(
+        image_batch=image_batch, box_batch=box_batch, point_batch=point_batch
+    )
+    writer.add_image("assign", plot, 1)
+    score_batch, box_batch, label_batch = model.to_boxes(pred_yolo_batch)
+    plot = batch_draw(image_batch=image_batch, box_batch=box_batch)
+    writer.add_image("assign", plot, 2)
+    writer.flush()
